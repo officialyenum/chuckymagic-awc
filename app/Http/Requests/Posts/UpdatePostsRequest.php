dedff553cee1fb5e80a0ead5c776f081d29c4e9c
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-05-10 21:07:03 +0100

    max size

--- app/Http/Requests/Posts/UpdatePostsRequest.php
+++ app/Http/Requests/Posts/UpdatePostsRequest.php
@@ -26,7 +26,7 @@ class UpdatePostsRequest extends FormRequest
         return [
             'title' => 'required',
             'description' => 'required',
-            'image' => 'image|mimes:jpg,jpeg,png|size:2048',
+            'image' => 'image|mimes:jpg,jpeg,png|max:5120',
             'content' => 'required',
             'category' => 'required'
         ];
