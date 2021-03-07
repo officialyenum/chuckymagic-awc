72d13383e1668152aa10886fb03cd66272c22386
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2021-03-07 11:33:10 +0100

    update register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
